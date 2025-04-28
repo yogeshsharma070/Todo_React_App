48fd1c3e3e62ac17404b14561eda15f5056a98da
Yogesh Sharma <yogeshsharma11007@gmail.com>
2025-04-28 13:40:35 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { v4 as uuidv4 } from 'uuid';
+import './App.css'
+import './index.css'
+
+function App() {
+
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const [showFinished, setshowFinished] = useState(true)
+
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if(todoString){
+      let todos = JSON.parse(localStorage.getItem("todos")) 
+      setTodos(todos)
+    }
+  }, [])
+  
+
+  const saveToLS = (params) => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished)
+  }
+  
+  
+
+
+  const handleEdit = (e, id)=>{ 
+    let t = todos.filter(i=>i.id === id) 
+    setTodo(t[0].todo)
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    }); 
+    setTodos(newTodos) 
+    saveToLS()
+  }
+
+  const handleDelete= (e, id)=>{  
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    }); 
+    setTodos(newTodos) 
+    saveToLS()
+  }
+
+  const handleAdd= ()=>{
+    setTodos([...todos, {id: uuidv4(), todo, isCompleted: false}])
+    setTodo("") 
+    saveToLS()
+  }
+  
+  const handleChange= (e)=>{ 
+    setTodo(e.target.value)
+  }
+
+  const handleCheckbox = (e) => { 
+    let id = e.target.name;  
+    let index = todos.findIndex(item=>{
+      return item.id === id;
+    }) 
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+    saveToLS()
+  }
+  
+
+  return (
+    <>
+       <div className="container mx-auto my-10 rounded-xl p-5  min-h-[87vh] max-w-[70vw] ">
+        <h1 className='head font-bold text-center text-3xl '><span className='text-white'>eTask</span> - Simplify Your Work</h1>
+         <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className='text-2xl font-bold'>Add a Todo</h2>
+          <div className="flex">
+
+          <input  placeholder="More than 5 characters" onChange={handleChange} value={todo} type="text" className='w-full rounded-full px-5 py-1' />
+          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-black mx-2 rounded-full hover:bg-gray-700 p-4 py-2 text-sm font-bold text-white cursor-pointer'>Save</button>
+          </div>
+         </div>
+         <input className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} /> 
+         <label className='mx-2' htmlFor="show">Show Finished</label> 
+         <hr></hr>
+         <div className='h-[1px] bg-black opacity-15 w-[90%] mx-auto my-2'></div>
+         <h2 className='text-2xl font-bold'>Your Todos</h2>
+         <div className="todos">
+          {todos.length ===0 && <div className='m-5'>No Todos to display</div> }
+          {todos.map(item=>{
+ 
+          return (showFinished || !item.isCompleted) && <div key={item.id} className={"todo flex my-3 justify-between font-bold text-lg"}>
+            <div className='flex gap-5'> 
+            <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
+            <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+            </div>
+            <div className="buttons flex h-full">
+              <button onClick={(e)=>handleEdit(e, item.id)} className='bg-black hover:bg-transparent p-2 py-1 text-sm font-bold text-white rounded-md mx-1'><FaEdit /></button>
+              <button onClick={(e)=>{handleDelete(e, item.id)}} className='bg-black hover:bg-transparent p-2 py-1 text-sm font-bold text-white rounded-md mx-1'><AiFillDelete /></button>
+            </div> 
+          </div>
+          })}
+         </div>
+        
+       </div>
+    </>
+  )
+}
+
+export default App
